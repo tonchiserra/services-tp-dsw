d8bb6021fa0e8d5b68dee1fec5b5107a0e244b45
unknown <guidobitti@gmail.com>
2023-08-16 18:12:13 -0300

    Borrar archivos .sass

--- Frontend/src/app/home-page/home-page.component.ts
+++ Frontend/src/app/home-page/home-page.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.sass']
 })
 export class HomePageComponent {
 
