08612a62c2feba7699a1a7d711583028c5d84bc2
tonchiserra <tonchiserra@gmail.com>
2023-09-14 20:37:20 -0300

    submenu inside menu

--- Frontend/src/app/header/main-header.component.ts
+++ Frontend/src/app/header/main-header.component.ts
@@ -5,5 +5,13 @@ import { Component } from '@angular/core';
   templateUrl: './main-header.component.html'
 })
 export class MainHeaderComponent {
+  showSubmenu(event: MouseEvent) {
+    let button = (event.target as HTMLElement).closest('button')
+    if(!button) return
 
+    let submenu = button.nextElementSibling
+    if(!submenu) return
+    
+    submenu.classList.toggle('open')
+  }
 }
