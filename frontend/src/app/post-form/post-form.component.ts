6c39defd34a10585953fdd051f8128f6623f5e6b
tonchiserra <tonchiserra@gmail.com>
2023-09-19 20:30:57 -0300

    add events to postForm and others

--- frontend/src/app/post-form/post-form.component.ts
+++ frontend/src/app/post-form/post-form.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
 
-import { IPostForm } from '../../interfaces/PostFormInterface';
+import { IPostForm, IPostFormData } from '../../interfaces/PostFormInterface';
 
 @Component({
   selector: 'post-form',
@@ -9,7 +9,53 @@ import { IPostForm } from '../../interfaces/PostFormInterface';
 })
 export class PostFormComponent {
   postFormState: IPostForm = {
-    isServicePost: new FormControl(false)
+    isServicePost: new FormControl(false),
+    service: new FormControl(''),
+    text: new FormControl(''),
+    files: new FormControl()
   };
+
+  formMedias = ''
+
+  uploadMedia(event: MouseEvent): void {
+    event.preventDefault()
+    event.stopPropagation()
+    
+    let inputMedia = document.querySelector<HTMLElement>('.form-post input[type="file"]')
+    if(!inputMedia) return
+
+    inputMedia.click()
+  }
+
+  showMediaThumbnails(event: Event): void {
+    if(!event.target) return
+
+    let selectedFiles = (event.target as HTMLInputElement).files || []
+    if(selectedFiles.length <= 0) return
+
+    this.formMedias = ''
+
+    for(let i=0; i < selectedFiles.length; i++) {
+      let file = selectedFiles[i]
+
+      this.formMedias += (i === selectedFiles.length-1) ? file.name : `${file.name}, `
+    }
+  }
+
+  submitHandler(event: Event) {
+    event.preventDefault()
+    event.stopPropagation()
+    
+    const data: IPostFormData = {
+      isServicePost: this.postFormState.isServicePost.value || false,
+      service: this.postFormState.service.value || '',
+      text: this.postFormState.text.value || '',
+      files: this.postFormState.files.value
+    }
+
+    console.log(data)
+
+    // enviar datos al backend
+  }
 
 }
--- frontend/src/interfaces/PostFormInterface.ts
+++ frontend/src/interfaces/PostFormInterface.ts
@@ -1,5 +1,15 @@
 import { FormControl } from '@angular/forms';
 
 export interface IPostForm {
-    isServicePost: FormControl<boolean | null>
+    isServicePost: FormControl<boolean | null>,
+    service: FormControl<string | null>,
+    text: FormControl<string | null>,
+    files: FormControl<FileList | null>
+}
+
+export interface IPostFormData {
+    isServicePost: boolean,
+    service: string,
+    text: string,
+    files: FileList | null
 }
