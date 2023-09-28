5912de461c7d45700b48b17bf068c20f8c302732
tonchiserra <tonchiserra@gmail.com>
2023-09-28 00:16:45 -0300

    validate errors with zod

---
+++ frontend/src/app/login-page/login-page.interface.ts
@@ -0,0 +1,12 @@
+export interface ISignUpForm {
+    name:               string
+    username:           string
+    email:              string
+    password:           string
+    passwordRepeat:     string
+}
+
+export interface ISignInForm {
+    email:              string
+    password:           string
+}
