f7c3fb217776d03b9163646a8b261c9bbf495466
tonchiserra <tonchiserra@gmail.com>
2023-12-05 21:55:28 -0300

    confirmation popup

--- frontend/src/app/header/main-header.component.ts
+++ frontend/src/app/header/main-header.component.ts
@@ -10,6 +10,9 @@ export class MainHeaderComponent {
 
   auth = this.authService
   profileURL = '/user/'
+  confirmationMessage = ''
+  showConfirmationModal = false
+  currentConfirmationModal = ''
 
   ngOnInit() {
     this.authService.getUserLogged().subscribe(
@@ -31,4 +34,20 @@ export class MainHeaderComponent {
     
     submenu.classList.toggle('open')
   }
+
+  handleConfirmationModal(event: string) {
+    if(event === 'logout') this.confirmationMessage = 'Are you sure you want to log out?'
+    
+    if(event === 'delete') this.confirmationMessage = 'Are you sure you want to delete your account?'
+
+    if(event === 'confirmation') {
+      if(this.currentConfirmationModal === 'logout') this.auth.logOut()
+      if(this.currentConfirmationModal === 'delete') this.auth.deleteAccount()
+    }
+
+    this.showConfirmationModal = !this.showConfirmationModal
+    this.currentConfirmationModal = event
+  }
+
+
 }
